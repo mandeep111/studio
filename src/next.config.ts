75629293e41948fe59941222f55b89e6b855e594
Mandeep Dhakal <mandeepdhakal11@gmail.com>
2025-07-05 04:43:45 +0000

    Error:  ⚠ Blocked cross-origin request from 9000-firebase-studio-1750998

--- src/next.config.ts
+++ src/next.config.ts
@@ -3,8 +3,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["https://*.cloudworkstations.dev","*.cloudworkstations.dev",
-    'https://9000-firebase-studio-1750998323126.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev'
+  allowedDevOrigins: [
+    "https://*.cloudworkstations.dev",
+    "https://6000-firebase-studio-1750998323126.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev",
+    "https://9000-firebase-studio-1750998323126.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"
   ],
   eslint: {
     ignoreDuringBuilds: true,
